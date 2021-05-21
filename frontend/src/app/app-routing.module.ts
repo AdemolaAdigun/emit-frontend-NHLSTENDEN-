822337a3de0c77079e734920b6161ecb72d8e9d7
AdemolaAdigun <Adigun673@gmail.com>
2021-05-21 21:33:55 +0200

    added proper routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ProjectOverviewComponent } from './pages/project-overview/project-overview.component';
+import { InventoryOverviewComponent } from './pages/inventory-overview/inventory-overview.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { UserOverviewComponent } from './pages/user-overview/user-overview.component';
+import { AdminOverviewComponent } from './pages/admin-overview/admin-overview.component';
 
-// import { LoginComponent }
 
 const routes: Routes = [
-  // { path: 'login', component: null },
-  // { path: 'overview', component: null },
-  // { path: 'admin', component: null },
-  // { path: 'projects', component: null },
-  // { path: 'inventory', component: null },
-  // { path: 'shop', component: null },
+  { path: 'login', component: LoginComponent },
+  { path: 'project-overview', component: ProjectOverviewComponent },
+  { path: 'inventory-overview', component: InventoryOverviewComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'user', component: UserOverviewComponent },
+  { path: 'admin', component: AdminOverviewComponent },
 ];
 
 @NgModule({
