ea38282dad37e46deae49f7aa06e87b1ac23d7ad
simionpatrick <43606591+simionpatrick@users.noreply.github.com>
2021-06-07 18:54:01 +0300

    Categories added

--- frontend/src/app/pages/inventory-overview/inventory-overview.component.spec.ts
+++ frontend/src/app/pages/inventory-overview/inventory-overview.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { InventoryOverviewComponent } from './inventory-overview.component';
+import {InventoryOverviewComponent} from './inventory-overview.component';
 
 describe('InventoryOverviewComponent', () => {
   let component: InventoryOverviewComponent;
@@ -8,9 +8,9 @@ describe('InventoryOverviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InventoryOverviewComponent ]
+      declarations: [InventoryOverviewComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
